66867af160c64f1d6818e75a275fd6dee94178cb
Joseph Chiang <josephchiang28@gmail.com>
2016-03-15 02:56:16 -0700

    Now only exisiting user emails in db allowed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
   def create
     begin
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = @user.id
-      flash[:success] = 'Welcome, ' + @user.name
+      auth_hash = request.env['omniauth.auth']
+      @user = User.from_omniauth(auth_hash)
+      if @user.nil?
+        flash[:warning] = 'Error: Your email ' + auth_hash.info.email + ' is not in the white list.'
+      else
+        session[:user_id] = @user.id
+        flash[:success] = 'Welcome, ' + @user.name
+      end
     rescue Exception => e
       puts 'SIGN IN ERROR: ' + e.message
       flash[:warning] = 'Ooops. There was an error while trying to authenticate you...'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ActiveRecord::Base
   has_many :players
   validates :email, presence: true
 
+  # Only allow and update users that are manually entered by admin
   def self.from_omniauth(auth_hash)
-    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
-      user.email = auth_hash.info.email
+    user = User.where(email: auth_hash.info.email).first
+    if not user.nil?
       user.name = auth_hash.info.name
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
       user.save!
     end
+    user
   end
 end
